7ed04ccec49c2afb689b694d54c4f649e01af424
vigor-13 <vigor13.dev@gmail.com>
2022-06-05 15:07:48 +0900

    fix: modify auth page variant value check

--- pages/auth/[variant].page.tsx
+++ pages/auth/[variant].page.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import { NextPageWithLayout } from '@utils/types';
 import { AuthForm } from './_components';
+import { ParsedUrlQuery } from 'querystring';
 
 export interface AuthPageProps {
   variant?: 'login' | 'register';
@@ -9,12 +10,12 @@ export interface AuthPageProps {
 const AuthPage: NextPageWithLayout = (props: AuthPageProps) => {
   const router = useRouter();
   const { variant: _variant } = props; // from Component Props
-  let { variant } = router.query; // from Router
+  const query = router.query; // from Router
 
-  variant = variant ? variant : _variant;
-  if (variant !== 'login' && variant !== 'register') {
-    throw new Error('Invalid auth page query.');
-  }
+  const variant = isValidVariant(query)
+    ? (query.variant as 'login' | 'register')
+    : _variant;
+  if (!variant) return null;
 
   return (
     <div>
@@ -24,6 +25,10 @@ const AuthPage: NextPageWithLayout = (props: AuthPageProps) => {
       </div>
     </div>
   );
+};
+
+const isValidVariant = (query: ParsedUrlQuery): boolean => {
+  return query.variant === ('login' || 'register');
 };
 
 export default AuthPage;
